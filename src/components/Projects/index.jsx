98f0843231092de9330d6474d3672e205902d0d8
Wirley Almeida <wirley.s.almeida@gmail.com>
2022-03-29 23:42:20 -0300

    imagens2

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -6,31 +6,31 @@ const data = [
     id: '1',
     name: 'Front End I - Checkpoint 1',
     link: 'https://wirleysalmeida.github.io/checkpoint1FrontEndI/',
-    image: 'src/assets/checkpoint1feiipreview.png'
+    image: '../src/assets/checkpoint1feipreview.png'
   },
   {
     id: '2',
     name: 'Front End I - Checkpoint 2',
     link: 'https://wirleysalmeida.github.io/FE-checkpoint-2/',
-    image: 'src/assets/checkpoint2feipreview.png'
+    image: '../src/assets/checkpoint2feipreview.png'
   },
   {
     id: '3',
     name: 'Front End I - Checkpoint 3',
     link: 'https://wirleysalmeida.github.io/checkpoint-03-front-end/',
-    image: 'src/assets/checkpoint3feipreview.png'
+    image: '../src/assets/checkpoint3feipreview.png'
   },
   {
     id: '4',
     name: 'Front End II - Checkpoint 1',
     link: 'https://wirleysalmeida.github.io/checkpoint-front-end-2/',
-    image: 'src/assets/checkpoint1feiipreview.png'
+    image: '../src/assets/checkpoint1feiipreview.png'
   },
   {
     id: '5',
     name: 'Front End II - Checkpoint 2',
     link: 'https://wirleysalmeida.github.io/ctd_fe2_checkpoint2_update/',
-    image: 'src/assets/checkpoint2feiipreview.png'
+    image: '../src/assets/checkpoint2feiipreview.png'
   },
 ];
 
